a6b997ed0e212c238d96ba0b55b2274fec087d60
a-ataman <110892452+a-ataman@users.noreply.github.com>
2023-01-27 19:58:54 +0000

    Deploying to gh-pages from @ a-ataman/goit-js-hw-09@e0a166fe75f21764597c12e5ec5bc15a0cdd8229 🚀

---
+++ 01-color-switcher.d5d9d00f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n=null;d.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,d.disabled=!1,n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){e.disabled=!1,d.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.d5d9d00f.js.map
